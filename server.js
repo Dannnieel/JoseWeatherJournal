2ca26b4b8560423bb0bd81f00ea781fc61be37b7
Juan <jdosoriopu@gmail.com>
2023-08-17 09:14:50 -0500

    server.get

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('weather-journal-app/website'));
 
 // Setup Server
 const port = 3000;
